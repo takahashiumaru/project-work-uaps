7feab7dc67c487705f6e375ef946131c00791673
takahashiumaru <umarmarufmutaqin@gmail.com>
2025-09-18 12:05:52 +0700

    fix: ok

--- resources/views/training/admin/index.blade.php
+++ resources/views/training/admin/index.blade.php
@@ -12,7 +12,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <link rel="stylesheet" href="/assets/css/style.css">
-
     <style>
         /* Card Container */
         .card-container {
