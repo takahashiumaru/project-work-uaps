9faf48a3f59e4fc50e011166e03ab2e8ed91de13
faridjabbar <muhammad.jabbar@vneu.co.id>
2026-02-27 08:36:04 +0700

    ok

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,20 +11,42 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('is_active')->default(true);
+            $table->enum('gender', ['Male', 'Female']);
+            $table->string('job_title')->nullable();
             $table->string('role');
             $table->string('station');
-            $table->string('email')->unique();
-            $table->string('phone')->nullable();
-            $table->enum('gender', ['Male','Female']);
-            $table->string('password');
+            $table->string('cluster')->nullable();
+            $table->string('unit')->nullable();
+            $table->string('sub_unit')->nullable();
+            $table->string('status')->nullable();
+            $table->string('manager')->nullable();
+            $table->string('senior_manager')->nullable();
+            $table->boolean('is_qantas')->default(false);
             $table->date('join_date');
+            $table->string('salary')->default('0');
             $table->date('contract_start')->nullable();
             $table->date('contract_end')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('pendidikan')->nullable();
+            $table->date('tanggal_lahir')->nullable();
+            $table->string('tempat_lahir')->nullable();
+            $table->string('domisili')->nullable();
+            $table->text('alamat')->nullable();
+            $table->string('no_nik')->nullable();
+            $table->string('no_kk')->nullable();
+            $table->string('npwp')->nullable();
+            $table->string('no_pas')->nullable();
             $table->date('pas_registered')->nullable();
             $table->date('pas_expired')->nullable();
-            $table->string('salary')->default('0');
+            $table->string('bpjs_kesehatan')->nullable();
+            $table->string('bpjs_tk')->nullable();
+            $table->string('tim_number')->nullable();
+            $table->date('tim_registered')->nullable();
+            $table->date('tim_expired')->nullable();
             $table->string('profile_picture')->nullable();
-            $table->boolean('is_qantas')->default(false);
             $table->timestamps();
         });
 
@@ -51,4 +73,3 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
-
